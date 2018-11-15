eace35393d52c88b67bd824c70a46471ed8585d0
faide <faider29@mail.ru>
2018-11-15 18:32:50 +0300

    ввод телефона работает

--- app/src/main/java/com/example/faide/logscreen/MainActivity.java
+++ app/src/main/java/com/example/faide/logscreen/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.faide.logscreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText ET = findViewById(R.id.etpassnomb);
+        ET.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
     }
+
 }
